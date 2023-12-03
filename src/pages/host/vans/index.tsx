15728a61df9e3c92864e963510492ee59dffdfb8
DevNitro5 <anishchhetri196@gmail.com>
2023-12-03 08:10:55 +0000

    defer

--- src/pages/host/vans/index.tsx
+++ src/pages/host/vans/index.tsx
@@ -1,33 +1,43 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { Link, useLoaderData, defer, Await } from "react-router-dom";
 import type { LoaderFunctionArgs } from "react-router-dom";
 import { getHostVans } from "../../../utils/api";
 import { requireAuth } from "../../../utils/requireAuth";
+import { Van } from "../../vans";
+import React from "react";
 
 export async function loader({ request }: LoaderFunctionArgs) {
   await requireAuth(request);
-  return getHostVans();
+  return defer({ vans: getHostVans() });
 }
 
 export default function HostVans() {
-  const vans = useLoaderData() as Awaited<ReturnType<typeof loader>>;
+  const dataPromise = useLoaderData() as { vans: Promise<Van[]> };
 
-  const hostVansEls = vans.map((van) => (
-    <Link to={van.id} key={van.id} className="host-van-link-wrapper">
-      <div className="host-van-single" key={van.id}>
-        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
-        <div className="host-van-info">
-          <h3>{van.name}</h3>
-          <p>${van.price}/day</p>
+  function renderHostVans(vans: Van[]) {
+    const hostVansEls = vans.map((van) => (
+      <Link to={van.id} key={van.id} className="host-van-link-wrapper">
+        <div className="host-van-single" key={van.id}>
+          <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+          <div className="host-van-info">
+            <h3>{van.name}</h3>
+            <p>${van.price}/day</p>
+          </div>
         </div>
-      </div>
-    </Link>
-  ));
+      </Link>
+    ));
+
+    return hostVansEls;
+  }
 
   return (
     <section>
       <h1 className="host-vans-title">Your listed vans</h1>
       <div className="host-vans-list">
-        <section>{hostVansEls}</section>
+        <section>
+          <React.Suspense fallback={<h2>Loading.....</h2>}>
+            <Await resolve={dataPromise.vans}>{renderHostVans}</Await>
+          </React.Suspense>
+        </section>
       </div>
     </section>
   );
